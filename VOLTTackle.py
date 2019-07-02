bdfeb2c6287ecfb42903ed848c5083395bd368cf
HexadecimalMantis0e <32582986+HexadecimalMantis0e@users.noreply.github.com>
2019-07-01 20:02:44 -0700

    initial commit

---
+++ VOLTTackle.py
@@ -0,0 +1,107 @@
+import os
+import struct
+import argparse
+import zlib
+
+parser = argparse.ArgumentParser()
+parser.add_argument("directory")
+args = parser.parse_args()
+
+address = 0x00
+needPadCheck = False
+namelength = 0x00
+paddingsize = 0x00
+names = []
+
+print "Creating VOLT archive..."
+
+filelist = os.listdir(args.directory)
+f0 = open(args.directory+".vol","wb")
+number_files = len(filelist)
+
+for filename in os.listdir(args.directory):
+    namelength += len(filename) + 0x11
+    names += [filename]
+
+f0.write("VOLT")
+f0.write(struct.pack("i", 0x02))
+f0.write(struct.pack("i", number_files))
+f0.write(struct.pack("i", namelength))
+#f0.write(struct.pack("i", 0x00))
+
+# Handle pre filelist section
+for i in range(0,number_files):
+    namehash = zlib.crc32(names[i])
+    f0.write(struct.pack("i", namehash))
+    f0.write(struct.pack("i", 0x01))
+    f0.write(struct.pack("i", address))
+    address += (0x11 + len(names[i]))
+
+# Calculate main padding bytes
+padlength = 0x800 - (namelength + 0x10 + (number_files * 0x0C))
+
+startaddr = 0x800
+
+# Handle filelist section
+for filename in os.listdir(args.directory):
+    f0.write(struct.pack("i", startaddr))
+    f0.write(struct.pack("i", 0x00))
+
+    fpath = os.path.join(args.directory, filename)
+    f1 = open(fpath, "rb")
+
+    header = f1.read(4)
+
+    # pad everything that isn't a strat
+    if header != "BIGB":
+        needPadCheck = True
+
+    f1.seek(0x00, os.SEEK_END)
+    size = f1.tell()
+    f1.seek(0x00, os.SEEK_SET)
+
+    if needPadCheck == True:
+        print "Padding " + filename
+        paddingsize = 0x800 - (size % 0x800)
+        startaddr += paddingsize
+        needPadCheck = False
+    else:
+        print filename
+
+    f0.write(struct.pack("i", size))
+    f0.write(struct.pack("i", 0x00))
+    f0.write(filename)
+    f0.write(bytearray([0]))
+
+    startaddr += size
+    f1.close()
+
+
+f0.write(bytearray([0x69])*padlength)
+
+needPadCheck = False
+
+for filename in os.listdir(args.directory):
+    fpath = os.path.join(args.directory, filename)
+    f1 = open(fpath, "rb")
+
+    header = f1.read(4)
+
+    if header != "BIGB":
+        needPadCheck = True
+
+    f1.seek(0x00, os.SEEK_END)
+    size = f1.tell()
+    f1.seek(0x00, os.SEEK_SET)
+
+    filebytes = f1.read(size)
+    f0.write(filebytes)
+
+    if needPadCheck == True:
+        paddingsize = 0x800 - (size % 0x800)
+        f0.write(bytearray([0x69])*paddingsize)
+        needPadCheck = False
+
+    f1.close()
+
+f0.close()
